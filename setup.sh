292521b8c97a6078a07ecf508b47524af0aed891
KimWang906 <hyunbin06git@gmail.com>
2024-02-29 18:04:29 +0900

    UPDATE: cahnge binwalk url

--- setup.sh
+++ setup.sh
@@ -10,7 +10,8 @@ then
 fi
 
 echo "Installing binwalk"
-git clone --depth=1 https://github.com/ReFirmLabs/binwalk.git
+# git clone --depth=1 https://github.com/ReFirmLabs/binwalk.git
+git clone --depth=1 https://github.com/KimWang906/binwalk.git
 cd binwalk
 
 # Temporary fix for sasquatch failing to install (From https://github.com/ReFirmLabs/binwalk/pull/601)
